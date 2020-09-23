5cda0f3769df73e3323f0cbb57a6e3a457a7c032
Takasakiii <lucasmc2709@live.com>
2020-09-22 22:20:55 -0300

    Esquelto do projeto

---
+++ zuraaa-front-end/src/App.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+    #app
+        Navbar
+        view-router
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+import Navbar from './components/Navbar.vue'
+
+@Component({
+    components: {
+        Navbar
+    }
+})
+export default class App extends Vue{
+
+}
+</script>
+
---
+++ zuraaa-front-end/src/components/Navbar.vue
@@ -0,0 +1,77 @@
+<template lang="pug">
+    nav.navbar(role="navgation" aria-label="main-navigation")
+        .container
+            .navbar-brand
+                a.navbar-item(href="#/")
+                    img(src="../assets/imgs/logo.png" alt="Logo do bots para discord" width="65px" height="65px")
+            
+                a.navbar-burger(role="button" aria-label="menu" aria-expanded="false" :class="{'is-active': isCompact}" @click="switchCompact" data-target="navbar-content")
+                    span
+                    span
+                    span
+            #navbar-content.navbar-menu(:class="{'is-active': isCompact}")
+                .navbar-start
+                    a.navbar-item(href="#/")
+                        |Início
+                    a.navbar-item(href="#/")
+                        |Bots 
+                    a.navbar-item(href="#/")
+                        |Servidor
+                    a.navbar-item(href="#/")
+                        |Documentação
+                    a.navbar-item(href="#/")
+                        |Buscar
+                .navbar-end
+                    a.navbar-item(href="#/")
+                        |Logar
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+@Component
+export default class Navbar extends Vue{
+    private isCompact = false
+
+    private switchCompact(){
+        this.isCompact = !this.isCompact
+    }
+}
+</script>
+
+<style scoped>
+    .navbar{
+        background-color: #490e2d;
+        box-shadow: 0 4px #710f4b;
+    }
+
+        .navbar-burger > span {
+            color: white;
+        }
+
+    .navbar-item{
+        color: white;
+    }
+
+        .navbar-item:hover{
+            color: black;
+            transition-duration: 300ms;
+        }
+
+    .navbar-start {
+        justify-content: center;
+        width: 100%;
+    }
+
+    @media screen and (max-width: 1023px){
+        .navbar-menu{
+            background-color: rgb(0, 0, 0, 0.6);
+        }
+    }
+
+    .navbar-end > .navbar-item{
+        min-width: 100px;
+        justify-content: center;
+    }
+</style>
